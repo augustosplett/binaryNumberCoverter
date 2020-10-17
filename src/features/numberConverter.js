bd10640d3f1fcc503787321693103c9d9722004f
Augusto Madeira Splett <augusto.splett@gmail.com>
2020-10-17 11:16:22 -0300

    Inserida a função de exponenciação que será a base para o conversor de números binários

--- src/features/numberConverter.js
+++ src/features/numberConverter.js
@@ -3,5 +3,14 @@ const formulario = document.querySelector('[form-number-converter]');
 formulario.addEventListener("submit", transforma =>{
     transforma.preventDefault()
     const x = transforma.target.querySelector('[binary-input]').value
-    transforma.target.querySelector('[decimal-output]').value = x
+    transforma.target.querySelector('[decimal-output]').value = converte(x)
 })
+
+function converte(numero){
+    const arruma = numero.split('').reverse().join('')
+    let acumulador = 0
+    for(const i in arruma){
+       acumulador += (Math.pow(2, i)) * arruma[i]
+    }
+    return acumulador
+}   
