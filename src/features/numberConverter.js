b8ac89c3d2b42b86e73ac5eaf951187122d0446f
Augusto Madeira Splett <augusto.splett@gmail.com>
2020-10-17 21:33:39 -0300

    This is the final commit, all the user stories criteria have been achieved

--- src/features/numberConverter.js
+++ src/features/numberConverter.js
@@ -3,14 +3,21 @@ const formulario = document.querySelector('[form-number-converter]');
 formulario.addEventListener("submit", transforma =>{
     transforma.preventDefault()
     const x = transforma.target.querySelector('[binary-input]').value
-    transforma.target.querySelector('[decimal-output]').value = converte(x)
+    const maiorElemento = Math.max(...x.split(''))
+    if(maiorElemento == 0 || maiorElemento == 1){
+        transforma.target.querySelector('[decimal-output]').value = parseInt(x, 2);
+    }else{
+        alert("Favor informar apenas 0 e 1");
+        transforma.target.querySelector('[binary-input]').value = null;
+    }
+   
 })
 
-function converte(numero){
-    const arruma = numero.split('').reverse().join('')
-    let acumulador = 0
-    for(const i in arruma){
-       acumulador += (Math.pow(2, i)) * arruma[i]
-    }
-    return acumulador
-}   
+//function converte(numero){
+//   const arruma = numero.split('').reverse().join('')
+//    let acumulador = 0
+//    for(const i in arruma){
+//       acumulador += (Math.pow(2, i)) * arruma[i]
+//    }
+//    return acumulador
+//}   
